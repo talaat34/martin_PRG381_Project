5ff807e5d1f59a07aeb8d095847cc68f6ecaf431
MartinStudentProfile <576021@student.belgiumcampus.ac.za>
2021-02-24 11:10:25 +0200

    remove comments

--- GitTest.java
+++ GitTest.java
@@ -6,8 +6,7 @@ public class GitTest
     public static void main(String[] args) 
     {
         System.out.println("Hello form git");
-        //comment
-        //secondComment
+        
         System.out.println("Change 2");
         //Working on develop branch
     }
